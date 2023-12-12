504ce9f190c941ffba9e903c084215c0c0649174
jinuemong <jinwoo3661@naver.com>
2023-12-13 01:34:18 +0900

    [Feat]: 사용자 입력을 받는 Input 기능 구현

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,4 +1,18 @@
 package baseball.view
 
+import baseball.util.Message
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
+
+    fun enterNumber(): String {
+        print(Message.GAME_NUM_INPUT)
+        return Console.readLine()
+    }
+
+    fun enterGameRestart(): String {
+        println(Message.GAME_RESTART)
+        return Console.readLine()
+    }
+
 }
