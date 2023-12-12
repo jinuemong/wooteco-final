1144346f67e68361e199bac6431d7d24df6742b3
jinuemong <jinwoo3661@naver.com>
2023-12-13 01:32:26 +0900

    [Feat]: 메시지를 출력하는 output 기능 구현

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,4 +1,16 @@
 package baseball.view
 
+import baseball.util.Message
+
 class OutputView {
+
+    fun enterGame() = println(Message.GAME_START)
+
+    fun resultBailAndStrike(ball: Int, strike: Int) = println(Message.RESULT_STRIKE_AND_BALL.format(ball, strike))
+
+    fun resultBall(ball: Int) = println(Message.RESULT_ONLY_BALL.format(ball))
+
+    fun resultStrike(strike: Int) = println(Message.RESULT_ONLY_STRIKE.format(strike))
+
+    fun endGame() = println(Message.GAME_END)
 }
