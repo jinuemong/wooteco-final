a3cf31f4992f9b31aa10dfe4f04a302050e2d755
jinuemong <jinwoo3661@naver.com>
2023-12-13 01:27:40 +0900

    [Feat]: output 관련 Message 관리 오브젝트 구현

---
+++ src/main/kotlin/baseball/util/Message.kt
@@ -0,0 +1,13 @@
+package baseball.util
+
+object Message {
+
+    const val GAME_START = "숫자 야구 게임을 시작합니다."
+    const val GAME_NUM_INPUT = "숫자를 입력해주세요 : "
+    const val RESULT_STRIKE_AND_BALL = "%d볼 %d스트라이크"
+    const val RESULT_ONLY_BALL = "%d볼"
+    const val RESULT_ONLY_STRIKE = "%d스트라이크"
+    const val GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+
+}
