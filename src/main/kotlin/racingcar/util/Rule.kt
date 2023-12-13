6884053049989944fecbc89f886cf428ed4af43b
jinuemong <jinwoo3661@naver.com>
2023-12-13 19:01:29 +0900

    [Feat]: 사용자 입력에 대한 유효성 확인 기능 구현

--- src/main/kotlin/racingcar/util/Rule.kt
+++ src/main/kotlin/racingcar/util/Rule.kt
@@ -2,10 +2,12 @@ package racingcar.util
 
 object Rule {
 
+    const val MIN_NAME = 1
     const val MAX_NAME = 5
+    const val MINIMUM_NUMBER = 1
 
     const val MIN_RANDOM_NUM = 0
-    const val MAX_RANDOM_NUM = 0
+    const val MAX_RANDOM_NUM = 9
     const val MIN_PROCESS_NUM = 4
 
 }
--- src/main/kotlin/racingcar/validation/InputValidation.kt
+++ src/main/kotlin/racingcar/validation/InputValidation.kt
@@ -1,8 +1,19 @@
 package racingcar.validation
 
+import racingcar.util.Rule
 import java.lang.NumberFormatException
 
 class InputValidation {
+
+    fun checkAccuracyCarNames(userInput: String){
+        userInput.split(",").forEach {
+            require(isCorrectNameLength(it) && !isNumber(userInput))
+        }
+    }
+
+    fun checkAccuracyRacingNumber(userInput: String){
+        require(isNumber(userInput) && isProgress(userInput.toInt()))
+    }
 
 
     private fun isNumber(userInput: String): Boolean {
@@ -14,5 +25,11 @@ class InputValidation {
         }
     }
 
-    private fun isPositive(userInput: Int): Boolean = userInput > 0
+    private fun isProgress(userInput: Int): Boolean = userInput >= Rule.MINIMUM_NUMBER
+
+
+    private fun isCorrectNameLength(name: String): Boolean =
+        name.length in Rule.MIN_NAME..Rule.MAX_NAME
+
+
 }
