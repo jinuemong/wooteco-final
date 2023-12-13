315ad69ab413baeade7d4f12d2e0ca0fc4b72cce
jinuemong <jinwoo3661@naver.com>
2023-12-13 18:09:46 +0900

    [Feat]: 사용자에게 보여줄 메시지를 관리할 클래스 구현

---
+++ src/main/kotlin/racingcar/util/Message.kt
@@ -0,0 +1,10 @@
+package racingcar.util
+
+object Message {
+
+    const val GAME_START = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    const val TYPE_COUNT = "시도할 횟수는 몇 회인가요?"
+
+    const val RESULT_START = "실행 결과"
+    const val FINAL_WINNERS = "최종 우승자 : %s"
+}
