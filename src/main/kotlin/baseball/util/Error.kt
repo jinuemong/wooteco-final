9c8731b95e3b6f658af62800816ea5974112ac24
jinuemong <jinwoo3661@naver.com>
2023-12-13 15:41:55 +0900

    [Style]: 코드 정렬, 불필요 import 삭제

--- src/main/kotlin/baseball/util/Error.kt
+++ src/main/kotlin/baseball/util/Error.kt
@@ -6,8 +6,8 @@ object Error {
 
     const val NOT_VALID_NUMBER = "$ERR_MSG 유효하지 않은 입력값 입니다."
 
-    const val NOT_VALID_RESTART = "$ERR_MSG 1또는 2를 입력해야 합니다."
+    const val NOT_VALID_RESTART = "$ERR_MSG ${GameRule.RESTART_OK_CODE}또는 ${GameRule.RESTART_NO_CODE}를 입력해야 합니다."
 
-    const val NOT_NUMBER_COUNT = "$ERR_MSG 3가지 숫자를 입력해야 합니다."
+    const val NOT_NUMBER_COUNT = "$ERR_MSG ${GameRule.NUMBER_SIZE}가지 숫자를 입력해야 합니다."
 
 }
