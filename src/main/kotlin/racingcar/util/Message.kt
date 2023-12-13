a77f413ed0a4b04d14f8fdff6097c79cc15c8530
jinuemong <jinwoo3661@naver.com>
2023-12-13 18:28:08 +0900

    [Feat]: 사용자의 입력을 받는 Input 기능 구현

--- src/main/kotlin/racingcar/util/Message.kt
+++ src/main/kotlin/racingcar/util/Message.kt
@@ -4,7 +4,7 @@ object Message {
 
     const val GAME_START =
         "경주할 자동차 이름을 입력하세요.(이름은 ${Form.SEPARATOR_NAME}(${Form.SEPARATOR}) 기준으로 구분)"
-    const val TYPE_COUNT = "시도할 횟수는 몇 회인가요?"
+    const val TYPE_NUMBER = "시도할 횟수는 몇 회인가요?"
 
     const val RESULT_START = "실행 결과"
     const val FINAL_WINNERS = "최종 우승자${Form.CONNECTOR}%s"
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,4 +1,19 @@
 package racingcar.view
 
+
+import camp.nextstep.edu.missionutils.Console
+import racingcar.util.Message
+
 class InputView {
+
+    fun inputCarNames(): String{
+        println(Message.GAME_START)
+        return Console.readLine()
+    }
+
+    fun inputNumber(): String {
+        println(Message.TYPE_NUMBER)
+        return Console.readLine()
+    }
+
 }
