e75311b7746151075ae4da0ef81bea0b50830e16
jinuemong <jinwoo3661@naver.com>
2023-12-13 12:33:12 +0900

    [Feat]: 값을 검증하는 클래스 기능 구현

---
+++ src/main/kotlin/baseball/vaildation/CheckInputValidation.kt
@@ -0,0 +1,23 @@
+package baseball.vaildation
+
+import java.lang.NumberFormatException
+
+class CheckInputValidation {
+
+    private fun checkIsValidNumber(userInput: String) {
+        require(isNumber(userInput) && isSingleDigit(userInput)) {
+
+        }
+    }
+
+    private fun isNumber(userInput: String): Boolean {
+        return try {
+            userInput.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
+    private fun isSingleDigit(userInput: String): Boolean = userInput.toInt() in 1..9
+}
