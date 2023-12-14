366b7a1bcc62870a6edaf0a4689fec68ff7e8b9a
jinuemong <jinwoo3661@naver.com>
2023-12-15 02:53:58 +0900

    [Feat]: 게임 내부 규칙을 관리하는 클래스 기능 구현

---
+++ src/main/kotlin/christmas/utils/Rule.kt
@@ -0,0 +1,27 @@
+package christmas.utils
+
+object Rule {
+
+    const val MAX_ORDER_COUNT = 20
+
+    const val CHRISTMAS_START_DISCOUNT = 1_000
+    const val CHRISTMAS_DAY_DISCOUNT = 100
+
+    const val WEEKDAY_DISCOUNT = 2_023
+    const val WEEKEND_DISCOUNT = 2_023
+
+    const val STAR_DISCOUNT = 1_000
+
+    const val MIN_GIVEAWAY_PRICE = 120_000
+
+    const val CURRENT_GIVEAWAY = "샴페인"
+    const val CURRENT_GIVEAWAY_COUNT = 1
+
+    const val MIN_STAR_BADGE_PRICE = 5_000
+    const val MIN_TREE_BADGE_PRICE = 10_000
+    const val MIN_SANTA_BADGE_PRICE = 20_000
+
+    const val START_DATE = 1
+    const val EDT_DATE = 31
+
+}
