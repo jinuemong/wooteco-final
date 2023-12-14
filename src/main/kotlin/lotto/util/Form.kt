823daa5b1c8db326cc85812b325f855673511ee9
jinuemong <jinwoo3661@naver.com>
2023-12-14 14:31:09 +0900

    [Feat]: 출력을 담당하는 outputView 기능 구현

--- src/main/kotlin/lotto/util/Form.kt
+++ src/main/kotlin/lotto/util/Form.kt
@@ -4,5 +4,7 @@ object Form {
 
     const val LOTTO_SEPARATOR = ", "
     const val WINNING_NUM_SEPARATOR = ","
-
+    const val FORM_START_LOTTO = "["
+    const val FORM_END_LOTTO = "]"
+    const val FORM_RESULT_COUNT = "개"
 }
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,28 @@
+package lotto.view
+
+import lotto.util.Form
+import lotto.util.Message
+
+class OutputView {
+
+    fun outputLottoCount(count: Int) {
+        println(Message.PURCHASE_LOTTO_COUNT.format(count))
+    }
+
+    fun outputLottoNumbers(lottoNumbers: String) {
+        println("${Form.FORM_START_LOTTO}${lottoNumbers}${Form.FORM_END_LOTTO}")
+    }
+
+    fun outputStartResult() {
+        println(Message.START_RESULT)
+    }
+
+    fun outputResult(message: String, result: Int) {
+        println("$message ${result}${Form.FORM_RESULT_COUNT}")
+    }
+
+    fun outputRate(rate: String){
+        println(Message.TOTAL_RATE.format(rate))
+    }
+
+}
