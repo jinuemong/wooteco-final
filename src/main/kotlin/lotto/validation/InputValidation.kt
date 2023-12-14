2e1319362eef25e91f0dca5ec32263a63a6ff79d
jinuemong <jinwoo3661@naver.com>
2023-12-14 15:06:03 +0900

    [Feat]: 로또 값을 검증하는 기능 구현

--- src/main/kotlin/lotto/validation/InputValidation.kt
+++ src/main/kotlin/lotto/validation/InputValidation.kt
@@ -1,18 +1,9 @@
 package lotto.validation
 
 import lotto.util.Rule
-import java.lang.NumberFormatException
 
 class InputValidation {
 
-    private fun checkIsNumber(userInput: String): Boolean {
-        return try {
-            userInput.toInt()
-            true
-        } catch (e: NumberFormatException) {
-            false
-        }
-    }
 
     private fun checkMinimumPrice(price: Int): Boolean{
         return price >= Rule.LOTTO_PRICE
---
+++ src/main/kotlin/lotto/validation/LottoValidation.kt
@@ -0,0 +1,46 @@
+package lotto.validation
+
+import lotto.util.Error
+import lotto.util.Form
+import lotto.util.Rule
+import java.lang.NumberFormatException
+
+class LottoValidation {
+
+    fun checkAccuracyLottoNumbers(input: String){
+        val numbers = input.split(Form.LOTTO_SEPARATOR)
+        require(checkLottoCount(numbers)){
+            Error.INCORRECT_LOTTO_COUNT
+        }
+        numbers.forEach {number->
+            require(checkIsNumber(number) && checkIsLottoNum(number.toInt())){
+                Error.INCORRECT_LOTTO_NUM
+            }
+        }
+    }
+
+    fun checkAccuracyBonusNumber(input: String){
+
+        require(checkIsNumber(input) && checkIsLottoNum(input.toInt())){
+            Error.INCORRECT_LOTTO_NUM
+        }
+    }
+
+    fun checkIsNumber(userInput: String): Boolean {
+        return try {
+            userInput.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
+    fun checkIsLottoNum(num: Int): Boolean{
+        return num in Rule.MIN_LOTTO_NUM..Rule.MAX_LOTTO_NUM
+    }
+
+    private fun checkLottoCount(numbers: List<String>): Boolean{
+        return numbers.size == Rule.LOTTO_COUNT
+    }
+
+}
