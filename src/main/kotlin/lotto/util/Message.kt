81c012b125d124072ec459c06df178e4e99b758b
jinuemong <jinwoo3661@naver.com>
2023-12-14 14:18:49 +0900

    [Feat]: 수익률 계산 메시지 기능 추가

--- src/main/kotlin/lotto/util/Message.kt
+++ src/main/kotlin/lotto/util/Message.kt
@@ -7,5 +7,6 @@ object Message {
     const val REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요."
     const val REQUEST_BONUS_NUM = "보너스 번호를 입력해 주세요."
     const val START_RESULT = "당첨 통계\n---"
+    const val TOTAL_RATE = "총 수익률은 %S%입니다."
 
 }
