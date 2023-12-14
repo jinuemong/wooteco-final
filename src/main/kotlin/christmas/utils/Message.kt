38b2b72dc443caeadf67ac9d67fd4c81d9350e25
jinuemong <jinwoo3661@naver.com>
2023-12-15 02:47:39 +0900

    [Feat]: 출력 관련 메시지를 관리하는 클래스 기능 구현

---
+++ src/main/kotlin/christmas/utils/Message.kt
@@ -0,0 +1,19 @@
+package christmas.utils
+
+object Message {
+
+    const val ORDER_START = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
+    const val INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+
+    const val INPUT_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+    const val ORDER_END ="12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
+
+    const val ORDER_MENU = "<주문 메뉴>"
+    const val ORDER_PRICE ="<할인 전 총주문 금액>"
+    const val GIVEAWAY_MENU ="<증정 메뉴>"
+    const val BENEFIT = "<혜택 내역>"
+    const val TOTAL_DISCOUNT_PRICE ="<총혜택 금액>"
+    const val DISCOUNTED_PRICE = "<할인 후 예상 결제 금액>"
+    const val EVENT_BADGE ="<12월 이벤트 배지>"
+
+}
