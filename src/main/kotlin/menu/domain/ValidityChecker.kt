d34092d9150e5facab8732fe7049a5264358280f
jinuemong <jinwoo3661@naver.com>
2023-12-15 21:38:49 +0900

    [Feat]: 이름값 검증 규칙 구현과 검증 로직 구현

--- src/main/kotlin/menu/domain/ValidityChecker.kt
+++ src/main/kotlin/menu/domain/ValidityChecker.kt
@@ -1,4 +1,30 @@
 package menu.domain
 
+import menu.domain.utils.Rule
+import java.lang.NumberFormatException
+
 class ValidityChecker {
+
+    private fun checkIsNumber(userInput: String): Boolean {
+        return try {
+            userInput.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
+    private fun checkMinName(name: String) : Boolean{
+        return name.length >= Rule.MIN_NAME
+    }
+
+    private fun checkMaxName(name: String) : Boolean {
+        return name.length <= Rule.MAX_NAME
+    }
+
+    private fun checkNameValid(name: String ): Boolean{
+        return name.matches(Regex(Rule.NAME_RULE))
+    }
+
+    private
 }
--- src/main/kotlin/menu/domain/utils/Rule.kt
+++ src/main/kotlin/menu/domain/utils/Rule.kt
@@ -5,5 +5,14 @@ object Rule {
     const val MIN_MENU_NUM = 1
     const val MAX_MENU_NUM = 5
 
+    const val MIN_NAME = 2
+    const val MAX_NAME = 4
+
+    const val MIN_COACH_NUM = 2
+    const val MAX_COACH_NUM = 5
+
     const val MAX_WEEKEND_CATEGORY = 2
+
+    const val NAME_RULE = "^[\\s가-힣a-zA-Z]{${MIN_NAME},${MAX_NAME}}\$"
+
 }
