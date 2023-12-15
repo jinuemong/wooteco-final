76112dfa7e3b81136e62bde9dcd1171936c88a59
jinuemong <jinwoo3661@naver.com>
2023-12-15 13:14:19 +0900

    [Feat]: 주문지를 확인해서 이벤트 일정별 할인을 체크하는 이벤트 플래너 기능 구현

--- src/main/kotlin/christmas/domain/EventPlanner.kt
+++ src/main/kotlin/christmas/domain/EventPlanner.kt
@@ -1,4 +1,40 @@
 package christmas.domain
 
-class EventPlanner {
+import christmas.domain.model.EventDayOfWeek
+import christmas.domain.model.MenuInfo
+import christmas.domain.model.MenuType
+import christmas.utils.Rule
+
+class EventPlanner(
+    private val eventCalendar: EventCalendar,
+    private val kiosk: Kiosk
+) {
+
+    fun computeDDay(date: Int): Int {
+        return if (eventCalendar.checkDDay(date)) {
+            Rule.CHRISTMAS_START_DISCOUNT + (date-1) * Rule.CHRISTMAS_DAY_DISCOUNT
+        } else {
+            0
+        }
+    }
+
+    fun computeBasicDay(date: Int): Int {
+        return when(eventCalendar.checkDayOfWeek(date)){
+            EventDayOfWeek.WEEKEND -> computeWeekend()
+            EventDayOfWeek.WEEKDAY -> computeWeekDay()
+        }
+    }
+
+    private fun computeWeekend(): Int{
+        return 0 // rngus
+    }
+
+    private fun computeWeekDay(): Int{
+        return 0 // rngus
+    }
+
+    companion object {
+        private val CURRENT_WEEKEND_MENU = MenuType.MAIN
+        private val CURRENT_WEEKDAY_MENU = MenuType.DESSERT
+    }
 }
