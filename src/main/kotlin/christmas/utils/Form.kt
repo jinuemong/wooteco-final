efe870e9611508ca7ad7227cc15f05f61432403c
jinuemong <jinwoo3661@naver.com>
2023-12-15 14:53:02 +0900

    [Feat]: 유저 입력 값 검증 기능 구현

--- src/main/kotlin/christmas/utils/Form.kt
+++ src/main/kotlin/christmas/utils/Form.kt
@@ -2,6 +2,8 @@ package christmas.utils
 
 object Form {
 
+    const val ORDER_SEPARATOR = "-"
+    const val MENU_SEPARATOR = ","
     const val UNIT_MENU = "개"
     const val UNIT_PRICE = "원"
     const val RETURN_FORMAT = "#,###.0"
--- src/main/kotlin/christmas/validation/UserValidation.kt
+++ src/main/kotlin/christmas/validation/UserValidation.kt
@@ -1,10 +1,33 @@
 package christmas.validation
 
 
+import christmas.domain.model.MenuInfo
+import christmas.utils.Form
 import christmas.utils.Rule
 import java.lang.NumberFormatException
 
 class UserValidation {
+
+    fun checkUserDate(userInput: String){
+        require(checkIsNumber(userInput) && checkIsDate(userInput.toInt()))
+    }
+
+    fun checkMenus(userInput: String){
+        require(userInput.isNotEmpty())
+
+        var menuCount = 0
+        userInput.split(Form.ORDER_SEPARATOR).forEach { pairMenu ->
+
+            require(checkPair(pairMenu))
+
+            val (menu,num) = pairMenu.split(Form.MENU_SEPARATOR)
+            require(checkIsMenu(menu))
+            require(checkIsNumber(num) && checkMenuRange(num.toInt()))
+
+            menuCount+= num.toInt()
+            require(checkMenuRange(menuCount))
+        }
+    }
 
     private fun checkIsNumber(userInput: String): Boolean {
         return try {
@@ -16,7 +39,25 @@ class UserValidation {
     }
 
     private fun checkIsDate(userInput: Int): Boolean {
-        return userInput in Rule.START_DATE.. Rule.END_DATE
+        return userInput in Rule.START_DATE..Rule.END_DATE
+    }
+
+
+    private fun checkIsMenu(name: String): Boolean{
+        return try {
+            MenuInfo.getMenuInfo(name)
+            true
+        } catch (e: IllegalArgumentException){
+            false
+        }
+    }
+
+    private fun checkMenuRange(count: Int): Boolean{
+        return count in 0..Rule.MAX_ORDER_COUNT
+    }
+
+    private fun checkPair(data: String): Boolean{
+        return data.split(Form.ORDER_SEPARATOR).size == 2
     }
 
 
