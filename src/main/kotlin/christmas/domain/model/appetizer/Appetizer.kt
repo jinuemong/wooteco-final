a813fa23fc7e4a8988f16abe2dc3aa122c5749c6
jinuemong <jinwoo3661@naver.com>
2023-12-15 12:10:17 +0900

    [Feat]: 애피타이저 메뉴 시리드 클래스 기능 구현

--- src/main/kotlin/christmas/domain/model/appetizer/Appetizer.kt
+++ src/main/kotlin/christmas/domain/model/appetizer/Appetizer.kt
@@ -1,13 +1,16 @@
 package christmas.domain.model.appetizer
 
 import christmas.domain.model.Menu
+import christmas.domain.model.MenuInfo
 import christmas.domain.model.MenuType
 
-sealed class Appetizer(menuType: MenuType): Menu(menuType) {
+sealed class Appetizer(
+    menuType: MenuType,
+    menuInfo: MenuInfo
+): Menu(menuType,menuInfo) {
 
-//    Mushroom Soup
-//    Tapas
-//    Caesar Salad
-
+    class MushroomSoup(menuType: MenuType,menuInfo:MenuInfo): Appetizer(menuType,menuInfo)
+    class Tapas(menuType: MenuType,menuInfo:MenuInfo): Appetizer(menuType,menuInfo)
+    class CaesarSalad(menuType: MenuType,menuInfo:MenuInfo): Appetizer(menuType,menuInfo)
 
 }
