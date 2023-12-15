a1ae9debe8af01df38dba08856f4cfd8037770d4
jinuemong <jinwoo3661@naver.com>
2023-12-15 20:38:39 +0900

    [Feat]: 이미 먹은 메뉴 제거 기능 구현

--- src/main/kotlin/menu/domain/model/Coach.kt
+++ src/main/kotlin/menu/domain/model/Coach.kt
@@ -1,15 +1,19 @@
 package menu.domain.model
 
 class Coach(
-    private val forbiddenFood : Set<String>,
+    private val forbiddenFood: Set<String>,
     private val menus: MutableList<String> = mutableListOf()
 ) {
 
-    fun saveMenu(menu: String){
+    fun saveMenu(menu: String) {
         menus.add(menu)
     }
 
-    fun checkIsForbiddenFood(menu:String): Boolean{
+    fun checkIsForbiddenFood(menu: String): Boolean {
         return forbiddenFood.contains(menu)
+    }
+
+    fun checkIsPrevFeed(menu: String): Boolean {
+        return menus.contains(menu)
     }
 }
