e148a860903d7f24a66be62f5b971acf5df6ac3c
jinuemong <jinwoo3661@naver.com>
2023-12-15 13:03:14 +0900

    [Feat]: 이벤트 일정을 확인할 수 있는 이벤트 캘린더 기능 구현

--- src/main/kotlin/christmas/domain/EventCalendar.kt
+++ src/main/kotlin/christmas/domain/EventCalendar.kt
@@ -1,4 +1,36 @@
 package christmas.domain
 
+import christmas.domain.model.EventDayOfWeek
+import christmas.utils.Rule
+
 class EventCalendar {
+
+    fun checkDDay(date: Int): Boolean {
+        return date in Rule.START_DATE..D_DAY
+    }
+
+    fun checkDayOfWeek(date: Int): EventDayOfWeek {
+        return if (date % WEEKEND_CYCLE == FIRST_WEEKEND ||
+            date % WEEKEND_CYCLE == SECOND_WEEKEND
+        ) EventDayOfWeek.WEEKEND
+        else EventDayOfWeek.WEEKDAY
+    }
+
+    fun checkStartDay(date: Int): Boolean {
+        return date % STAR_CYCLE == START_STAR_DAY || date == D_DAY
+    }
+
+
+
+    companion object {
+
+        private const val D_DAY = 25
+
+        private const val START_STAR_DAY = 3
+        private const val STAR_CYCLE = 7
+
+        private const val FIRST_WEEKEND = 1
+        private const val SECOND_WEEKEND = 2
+        private const val WEEKEND_CYCLE = 7
+    }
 }
