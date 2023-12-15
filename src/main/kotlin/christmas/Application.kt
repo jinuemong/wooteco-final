ba294c02f208fcfa8a1042f1231a172d92ce8d43
jinuemong <jinwoo3661@naver.com>
2023-12-15 14:26:42 +0900

    [Feat]: 모든 기능을 동작할 메인 컨트롤러 선언

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,13 @@
 package christmas
 
+import christmas.controller.EventController
+import christmas.view.InputView
+import christmas.view.OutputView
+
 fun main() {
-    TODO("프로그램 구현")
+    val eventController = EventController(
+        inputView = InputView(),
+        outputView = OutputView()
+    )
+    eventController.userFlow()
 }
---
+++ src/main/kotlin/christmas/controller/EventController.kt
@@ -0,0 +1,14 @@
+package christmas.controller
+
+import christmas.view.InputView
+import christmas.view.OutputView
+
+class EventController(
+    private val inputView: InputView,
+    private val outputView: OutputView
+) {
+
+    fun userFlow(){
+
+    }
+}
