27c6c131f72e273d3734543d3e418b535820838d
jinuemong <jinwoo3661@naver.com>
2023-12-15 13:28:39 +0900

    [Feat]: 할인 별 정보를 관리할 클래스 기능 구현

---
+++ src/main/kotlin/christmas/domain/model/BenefitInfo.kt
@@ -0,0 +1,11 @@
+package christmas.domain.model
+
+enum class BenefitInfo(
+    message: String
+) {
+    D_DAY_DISCOUNT("크리스마스 디데이 할인:"),
+    WEEKDAY_DISCOUNT("평일 할인:"),
+    WEEKEND_DISCOUNT("주말 할인:"),
+    SPECIAL_DISCOUNT("특별 할인:"),
+    GIVEAWAY_EVENT("증정 이벤트:");
+}
